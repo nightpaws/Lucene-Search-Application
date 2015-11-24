07f6b48d226b3a709c6e88da93e52a525cbffc8c
Nightpaws <nightpaws@users.noreply.github.com>
2015-11-24 15:49:58 +0000

    tabbing and stylesheet
    Added tabbing for moving from results to search and vice versa.
    Added stylesheet references and updated term for main to reflect new
    use of tabPane

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,23 +7,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.Pane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-        try {
-            Pane page = (Pane) FXMLLoader.load(Main.class.getResource("../view/mainWindow.fxml"));
-            Scene scene = new Scene(page);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("Lucene Search Group Project");
-            primaryStage.show();
-        } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+		try {
+			TabPane page = (TabPane) FXMLLoader.load(Main.class.getResource("../view/mainWindow.fxml"));
+			Scene scene = new Scene(page);
+			
+			scene.getStylesheets().add("view/style.css");
+			
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Lucene Search Group Project");
+			primaryStage.show();
+		} catch (Exception ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
