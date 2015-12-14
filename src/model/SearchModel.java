efc963fe6151851fb4f058b60dea03cb44c971e6
James Minney <j_minney@live.co.uk>
2015-12-14 14:54:50 +0000

    Model Code tidied

--- src/model/SearchModel.java
+++ src/model/SearchModel.java
@@ -105,75 +105,53 @@ public class SearchModel extends Observable implements ISearchModel {
 		IndexSearcher searcher = new IndexSearcher(reader);
 		Analyzer analyzer = new StandardAnalyzer();
 
-		queryString = searchTerm;
+		String line = searchTerm;
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 		QueryParser parser = new QueryParser(field, analyzer);
 
-		while (true) {
-			if (queries == null && queryString == null) {                        // prompt the user
-				System.out.println("Enter query: ");
-			}
+		line = line.trim();
 
-			String line = queryString != null ? queryString : in.readLine();
-
-			if (line == null || line.length() == -1) {
-				break;
+		String newLine = "*"+line+"*";
+		Query query = new WildcardQuery(new Term(field, newLine));
+		
+		/*if (repeat > 0) {                           // repeat & time as benchmark
+			Date start = new Date();
+			for (int i = 0; i < repeat; i++) {
+				searcher.search(query, 100);
 			}
+			Date end = new Date();
+			System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
+		}*/
 			
-			line = line.trim();
-			try {
-				if (line.length() == 0) {
-					break;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-			String newLine = "*"+line+"*";
-			//Query query = parser.parse(line);
-			Query query = new WildcardQuery(new Term(field, newLine));
-			System.out.println("Searching for: " + query.toString(field));
-
-			if (repeat > 0) {                           // repeat & time as benchmark
-				Date start = new Date();
-				for (int i = 0; i < repeat; i++) {
-					searcher.search(query, 100);
-				}
-				Date end = new Date();
-				System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
-			}
+		// Search the pages..
+		doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
 			
-			// Search the pages..
-			doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
-			
-			// Get Images from Page..
-			getImagesFromSearch(line);
-
-			
-			if (queryString != null) {
-				break;
-			}
-		}
+		// Get Images from Page..
+		getImagesFromSearch(line);
+		
 		reader.close();
 		setChanged();
 		notifyObservers();
-
 	}
-
+	
+	/**
+	 * Following a successful search, this method parses each document returned from
+	 * the search and adds the 'src' attribute of image elements that caused the
+	 * document to be matched to the searchResults Map. 
+	 * @param line
+	 * @throws IOException
+	 */
 	private void getImagesFromSearch(String line) throws IOException {
 		
-		System.out.println("Image Search Term: " + line);
 		for (Map<String, List<String>> m : searchResults) {
 			String file = m.get("Path").get(0);
 			List<String> imageSources = new ArrayList<String>();
-			
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			org.jsoup.nodes.Document htmldoc;
 			try {
 				StringBuilder sb = new StringBuilder();
 				String fileLine = br.readLine();
-				
 				while (fileLine != null) {
 				    sb.append(fileLine);
 				    sb.append(" ");
@@ -183,9 +161,7 @@ public class SearchModel extends Observable implements ISearchModel {
 			} finally {
 				br.close();
 			}
-			
-			
-			List<String> savedImageSource = new ArrayList<String>();
+		
 			// Get the img elements
 			Elements images = htmldoc.getElementsByTag("img");
 			
@@ -201,7 +177,6 @@ public class SearchModel extends Observable implements ISearchModel {
 					}
 				}
 			}
-			
 			m.put("imageSources", imageSources);
 		}
 		
@@ -211,9 +186,7 @@ public class SearchModel extends Observable implements ISearchModel {
 	@Override
 	public void videoSearch(String searchTerm) throws IOException, ParseException {
 		clearSearch();
-		/**
-		 * Currently a clone of title search
-		 */
+		
 		String field = "videoContent";
 		String queries = null;
 		int repeat = 0;
@@ -272,7 +245,8 @@ public class SearchModel extends Observable implements ISearchModel {
 			}
 		}
 		reader.close();
-
+		setChanged();
+		notifyObservers();
 	}
 	
 	@Override
@@ -323,7 +297,6 @@ public class SearchModel extends Observable implements ISearchModel {
 			line = line+"*";
 			//Query query = parser.parse(line);
 			Query query = new WildcardQuery(new Term(field, line));
-			System.out.println("Searching for: " + query.toString(field));
 
 			if (repeat > 0) {                           // repeat & time as benchmark
 				Date start = new Date();
@@ -343,7 +316,6 @@ public class SearchModel extends Observable implements ISearchModel {
 		reader.close();
 		setChanged();
 		notifyObservers();
-		
 	}
 
 	@Override
@@ -394,7 +366,6 @@ public class SearchModel extends Observable implements ISearchModel {
 			line = line+"*";
 			//Query query = parser.parse(line);
 			Query query = new WildcardQuery(new Term(field, line));
-			System.out.println("Searching for: " + query.toString(field));
 
 			if (repeat > 0) {                           // repeat & time as benchmark
 				Date start = new Date();
@@ -402,7 +373,6 @@ public class SearchModel extends Observable implements ISearchModel {
 					searcher.search(query, 100);
 				}
 				Date end = new Date();
-				System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
 			}
 
 			doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
@@ -418,10 +388,7 @@ public class SearchModel extends Observable implements ISearchModel {
 	
 	@Override
 	public void generalSearch(String searchTerm) throws IOException, ParseException {
-		System.out.println("SEARCHING");
 		clearSearch();
-
-		String index = "test_index";
 		String field = "generalContent";
 
 		String queries = null;
@@ -467,7 +434,6 @@ public class SearchModel extends Observable implements ISearchModel {
 			line = line+"*";
 			//Query query = parser.parse(line);
 			Query query = new WildcardQuery(new Term(field, line));
-			System.out.println("Searching for: " + query.toString(field));
 
 			if (repeat > 0) {                           // repeat & time as benchmark
 				Date start = new Date();
@@ -487,7 +453,6 @@ public class SearchModel extends Observable implements ISearchModel {
 		reader.close();
 		setChanged();
 		notifyObservers();
-		
 	}
 
 
@@ -527,7 +492,7 @@ public class SearchModel extends Observable implements ISearchModel {
 		System.out.println(numTotalHits + " total matching documents");
 
 		int start = 0;
-		int end = Math.min(numTotalHits, hitsPerPage);
+		int end = numTotalHits;
 		for (int i = start; i < end; i++) {
 			Document doc = searcher.doc(hits[i].doc);
 			String path = doc.get("path");
@@ -539,85 +504,5 @@ public class SearchModel extends Observable implements ISearchModel {
 				searchResults.add(resultMap);
 			}
 		}
-		while (true) {
-			if (end > hits.length) {
-				System.out.println("Only results 1 - " + hits.length +" of " + numTotalHits + " total matching documents collected.");
-				System.out.println("Collect more (y/n) ?");
-				String line = in.readLine();
-				if (line.length() == 0 || line.charAt(0) == 'n') {
-					break;
-				}
-
-				hits = searcher.search(query, numTotalHits).scoreDocs;
-			}
-
-			end = Math.min(hits.length, start + hitsPerPage);
-
-			for (int i = start; i < end; i++) {
-				if (raw) {                              // output raw format
-					System.out.println("doc="+hits[i].doc+" score="+hits[i].score);
-					continue;
-				}
-
-				Document doc = searcher.doc(hits[i].doc);
-				String path = doc.get("path");
-				if (path != null) {
-					System.out.println((i+1) + ". " + path);
-					String title = doc.get("title");
-					if (title != null) {
-						System.out.println("   Title: " + doc.get("title"));
-					}
-				} else {
-					System.out.println((i+1) + ". " + "No path for this document");
-				}
-
-			}
-
-			if (!interactive || end == 0) {
-				break;
-			}
-
-			if (numTotalHits >= end) {
-				boolean quit = false;
-				while (true) {
-					System.out.print("Press ");
-					if (start - hitsPerPage >= 0) {
-						System.out.print("(p)revious page, ");  
-					}
-					if (start + hitsPerPage < numTotalHits) {
-						System.out.print("(n)ext page, ");
-					}
-					System.out.println("(q)uit or enter number to jump to a page.");
-
-					String line = in.readLine();
-					if (line.length() == 0 || line.charAt(0)=='q') {
-						quit = true;
-						break;
-					}
-					if (line.charAt(0) == 'p') {
-						start = Math.max(0, start - hitsPerPage);
-						break;
-					} else if (line.charAt(0) == 'n') {
-						if (start + hitsPerPage < numTotalHits) {
-							start+=hitsPerPage;
-						}
-						break;
-					} else {
-						int page = Integer.parseInt(line);
-						if ((page - 1) * hitsPerPage < numTotalHits) {
-							start = (page - 1) * hitsPerPage;
-							break;
-						} else {
-							System.out.println("No such page");
-						}
-					}
-				}
-				if (quit) break;
-				end = Math.min(numTotalHits, start + hitsPerPage);
-			}
-		}
 	}
-
-
-
 }
